85c50fa186636bae04a4d1fa94c9b270556ec4bf
Shinto Joseph <shintoishere@gmail.com>
2019-12-09 16:48:05 +0530

    Initial Commit

---
+++ RaySwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RaySwiftUi
+//
+//  Created by Shinto Joseph on 09/12/19.
+//  Copyright © 2019 Shinto Joseph. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
